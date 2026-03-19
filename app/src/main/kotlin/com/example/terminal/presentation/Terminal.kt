d6214cc877f8e3199b59ece4a8938f76349053d6
olyamilakina <olya_aparina@mail.ru>
2026-03-19 17:13:37 +0300

    add scaling

--- app/src/main/kotlin/com/example/terminal/presentation/Terminal.kt
+++ app/src/main/kotlin/com/example/terminal/presentation/Terminal.kt
@@ -1,38 +1,55 @@
 package com.example.terminal.presentation
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.terminal.domain.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(
     bars: List<Bar>,
     modifier: Modifier = Modifier
 ) {
+    var visibleBarsCount by remember { mutableIntStateOf(100) }
+    val transformableState = rememberTransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange)
+            .roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT, bars.size)
+    }
+
     Canvas(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
+            .transformable(transformableState)
     ) {
         val max = bars.maxOf { it.high }
         val min = bars.minOf { it.low }
-        val barWidth = size.width / bars.size
+        val barWidth = size.width / visibleBarsCount
         val pxPerPoint = size.height / (max - min)
-        bars.forEachIndexed { index, bar ->
+        bars.take(visibleBarsCount).forEachIndexed { index, bar ->
             drawLine(
                 color = Color.White,
                 start = Offset(
-                    x = index * barWidth,
+                    x = (size.width - index * barWidth),
                     y = (size.height - (bar.low - min) * pxPerPoint)
                 ),
                 end = Offset(
-                    x = index * barWidth,
+                    x = (size.width - index * barWidth),
                     y = (size.height - (bar.high - min) * pxPerPoint)
                 ),
                 strokeWidth = 1f
