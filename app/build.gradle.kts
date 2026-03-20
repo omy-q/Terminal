db32c2d291cf96dae38d3443ce0b31931cb6b93b
olyamilakina <olya_aparina@mail.ru>
2026-03-20 15:33:59 +0300

    use remember saveable with TerminalState

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.android.secrets.gradle)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.android.secrets.gradle)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
--- app/src/main/kotlin/com/example/terminal/domain/Bar.kt
+++ app/src/main/kotlin/com/example/terminal/domain/Bar.kt
@@ -1,9 +1,12 @@
 package com.example.terminal.domain
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
+import kotlinx.parcelize.Parcelize
 import java.util.Calendar
 
 @Immutable
+@Parcelize
 data class Bar(
     val open: Float,
     val close: Float,
@@ -11,4 +14,4 @@ data class Bar(
     val high: Float,
     val time: Long,
     val calendar: Calendar
-)
+) : Parcelable
